e49aad78dff1b641220045c4f5994fe2ade2b3bd
LittleT0fu <chinchanathavornkraiwit@gmail.com>
2023-07-31 17:12:25 +0700

    first commit

---
+++ sidenav.js
@@ -0,0 +1,22 @@
+ /* Open the sidenav */
+ function openNavAbout() {
+    document.getElementById("about").style.width = "100%";
+}
+function openNavSkill() {
+    document.getElementById("skill").style.width = "100%";
+}
+function openNavContact() {
+    document.getElementById("contact").style.width = "100%";
+}
+
+
+/* Close/hide the sidenav */
+function closeNavAbout() {
+    document.getElementById("about").style.width = "0";
+}
+function closeNavSkill() {
+    document.getElementById("skill").style.width = "0";
+}
+function closeNavContact() {
+    document.getElementById("contact").style.width = "0";
+}
